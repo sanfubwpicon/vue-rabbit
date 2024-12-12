9129412318ba25761fbc5e93f6567c82ca564e36
Youjzh <youyou78@qq.com>
2024-12-12 08:25:55 +0800

    elementPlus自动按需引入配置

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,16 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-// 别名设置
-import HelloWorld from '@/components/HelloWorld.vue'
 
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+   <div class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
 </template>
 
 <style scoped>
